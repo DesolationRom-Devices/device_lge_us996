46555a3322b07c77ec9abdde0d58f5186177662c
Jacob McSwain <jacob.a.mcswain@gmail.com>
2017-05-28 22:05:23 -0500

    h910->vs995

--- deso.mk
+++ deso.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/lge/h910/full_h910.mk)
+$(call inherit-product, device/lge/vs995/full_vs995.mk)
 
 # Inherit some common stuff.
 $(call inherit-product, vendor/deso/common.mk)
@@ -6,10 +6,11 @@ $(call inherit-product, vendor/deso/common.mk)
 # Overlays (inherit after vendor/aokp to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_NAME := deso_h910
+PRODUCT_NAME := deso_vs995
+
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="v20" \
-    PRODUCT_NAME="elsa_att_us" \
-    BUILD_FINGERPRINT="lge/elsa_att_us/elsa:7.0/NRD90M/162611117c45c:user/release-keys" \
-    PRIVATE_BUILD_DESC="elsa_att_us-user 7.0 NRD90M 162611117c45c release-keys"
+    PRODUCT_NAME="elsa_vzw_us" \
+    BUILD_FINGERPRINT="lge/elsa_vzw/elsa:7.0/NRD90M/1628409164d39:user/release-keys" \
+    PRIVATE_BUILD_DESC="elsa_vzw-user 7.0 NRD90M 1628409164d39 release-keys"
